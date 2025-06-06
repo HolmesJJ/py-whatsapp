5e88f5ec7d2fe101d56d0b1029b9848d8d8a8272
holmesjj <hujiajun19961024@gmail.com>
2025-06-06 22:04:28 +0800

    Update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pywhatkit
 import pandas as pd
 
@@ -8,8 +9,17 @@ def test():
     pywhatkit.sendwhatmsg_instantly('+65', 'Hi, how are you?', 15, True, 2)
 
 
+def get_folder_path():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    else:
+        return os.path.dirname(os.path.abspath(__file__))
+
+
 def run():
-    folder_path = os.path.dirname(os.path.abspath(__file__))
+    print('--- Start ---')
+    folder_path = get_folder_path()
+    print(f'--- {folder_path} ---')
     for filename in os.listdir(folder_path):
         if filename.endswith('.xlsx'):
             file_path = os.path.join(folder_path, filename)
@@ -22,11 +32,16 @@ def run():
                     for index, row in df.iterrows():
                         col2 = row.iloc[4]
                         col10 = row.iloc[8]
+                        print(f'---')
+                        print(f'Sending message to phone {col2} in 10 seconds:')
                         pywhatkit.sendwhatmsg_instantly(f'+65{col2}', col10, 10, True, 2)
-                        print('Phone:', col2, 'Message:', col10)
+                        print('Message:', col10)
+                        print(f'---')
             except Exception as e:
                 print(f'Read {filename} error: {e}')
+    print('--- End ---')
 
 
 if __name__ == '__main__':
+    # pyinstaller --onefile --console --hidden-import=pywhatkit --hidden-import=pandas --hidden-import=openpyxl main.py
     run()
